98c871ba4ff06e1b73fd2d5412f1934d1d2fee77
artemowkin <artyom.groshev2017@gmail.com>
2021-03-02 00:46:32 +0700

    Refactored get_env function

--- utils/settings.py
+++ utils/settings.py
@@ -9,12 +9,24 @@ def get_env(env_name: str, default: Any = "") -> Union[str, int]:
     variable doesn't exist
     """
     env_variable = os.getenv(env_name, default)
-    if isinstance(env_variable, str) and not env_variable:
+    _check_env_variable(env_name, env_variable)
+    env_variable = _format_env_variable(env_variable)
+    return env_variable
+
+
+def _check_env_variable(env_name: str, env_var: Any) -> None:
+    """Check is environment variable valid"""
+    if isinstance(env_var, str) and not env_var:
         raise ImproperlyConfigured(
             f"You need to set {env_name} environment variable"
         )
 
-    if isinstance(env_variable, str) and env_variable.isnumeric():
-        return int(env_variable)
 
-    return env_variable
+def _format_env_variable(env_var: Any) -> Any:
+    """Format environment variable to integer if it includes
+    only numbers
+    """
+    if isinstance(env_var, str) and env_var.isnumeric():
+        return int(env_var)
+
+    return env_var
