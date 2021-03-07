4ffb0390c76efb16725a4ed15ce0e48c869370e0
artemowkin <artyom.groshev2017@gmail.com>
2021-03-07 21:25:33 +0700

    Added ReDoc

--- yakutiatripp/urls.py
+++ yakutiatripp/urls.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
+    # Django Admin
     path('admin/', admin.site.urls),
+
+    # ReDoc
+    path('redoc/', TemplateView.as_view(
+        template_name='redoc.html',
+        extra_context={'schema-url': 'openapi-schema'}
+    ), name='redoc'),
 
     # Local
     path('api/news/', include('news.urls')),
